64eed6c4b8c9654602f1783ac97b700cda392af3
Za-salman <salman@geekinformatic.com>
2025-06-13 19:20:13 +0530

    changes

--- src/feature-module/laundryServices/addService.tsx
+++ src/feature-module/laundryServices/addService.tsx
@@ -97,7 +97,8 @@ export default function AddServicesTabContent() {
     if (res?.status === 200) {
       let link = route.services + `?token=${token}&partnerId=${partnerId}`;
       console.log(link,">>>> link")
-      navigate(route.services + `?token=${token}&partnerId=${partnerId}`);
+      // navigate(route.services + `?token=${token}&partnerId=${partnerId}`);
+      window.location.href = `${route.services}?token=${token}&partnerId=${partnerId}`;
     }
   };
 
